9a38587854fec790fe843616549bf0d4f36f225b
micbuffa <micbuffa@gmail.com>
2022-01-06 11:53:40 +0100

    premier commit du back end angular

---
+++ model/assignment.js
@@ -0,0 +1,16 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let AssignmentSchema = Schema({
+    id: Number,
+    dateDeRendu: Date,
+    nom: String,
+    rendu: Boolean
+});
+
+// C'est à travers ce modèle Mongoose qu'on pourra faire le CRUD
+// le premier paramètre est le nom de la collection. Notez que
+// Mongoose fait du "matching" et prend la collection dont le nom
+// est le plus proche (en gros, Assignment ou assignments va marcher)
+// Attention la distance syntaxique doit être "petite"
+module.exports = mongoose.model('assignments', AssignmentSchema);
---
+++ server.js
@@ -0,0 +1,62 @@
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+let assignment = require('./routes/assignments');
+
+let mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+//mongoose.set('debug', true);
+
+// remplacer toute cette chaine par l'URI de connexion à votre propre base dans le cloud s
+const uri = 'mongodb+srv://mb:toto@cluster0.xtr0u.mongodb.net/assignments?retryWrites=true&w=majority';
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify:false
+};
+
+mongoose.connect(uri, options)
+  .then(() => {
+    console.log("Connecté à la base MongoDB assignments dans le cloud !");
+    console.log("at URI = " + uri);
+    console.log("vérifiez with http://localhost:8010/api/assignments que cela fonctionne")
+    },
+    err => {
+      console.log('Erreur de connexion: ', err);
+    });
+
+// Pour accepter les connexions cross-domain (CORS)
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
+// Pour les formulaires
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+let port = process.env.PORT || 8010;
+
+// les routes
+const prefix = '/api';
+
+app.route(prefix + '/assignments')
+  .get(assignment.getAssignments)
+  .post(assignment.postAssignment)
+  .put(assignment.updateAssignment);
+
+app.route(prefix + '/assignments/:id')
+  .get(assignment.getAssignment)
+  .delete(assignment.deleteAssignment);
+
+
+// On démarre le serveur
+app.listen(port, "0.0.0.0");
+console.log('Serveur démarré sur http://localhost:' + port);
+
+module.exports = app;
+
+
