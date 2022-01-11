58a83fdcd1b197c9bb9acc2b250af59cf8cf3c71
micbuffa <micbuffa@gmail.com>
2022-01-11 15:13:55 +0100

    ajout de la pagination

--- model/assignment.js
+++ model/assignment.js
@@ -1,4 +1,6 @@
 let mongoose = require('mongoose');
+var aggregatePaginate = require("mongoose-aggregate-paginate-v2");
+
 let Schema = mongoose.Schema;
 
 let AssignmentSchema = Schema({
@@ -7,6 +9,8 @@ let AssignmentSchema = Schema({
     nom: String,
     rendu: Boolean
 });
+
+AssignmentSchema.plugin(aggregatePaginate);
 
 // C'est à travers ce modèle Mongoose qu'on pourra faire le CRUD
 // le premier paramètre est le nom de la collection. Notez que
--- routes/assignments.js
+++ routes/assignments.js
@@ -1,6 +1,7 @@
 let Assignment = require('../model/assignment');
 
 // Récupérer tous les assignments (GET)
+/*
 function getAssignments(req, res){
     Assignment.find((err, assignments) => {
         if(err){
@@ -9,6 +10,24 @@ function getAssignments(req, res){
 
         res.send(assignments);
     });
+}
+*/
+// Récupérer tous les assignments (GET) AVEC PAGINATION
+function getAssignments(req, res) {
+ var aggregateQuery = Assignment.aggregate();
+ 
+ Assignment.aggregatePaginate(aggregateQuery,
+   {
+     page: parseInt(req.query.page) || 1,
+     limit: parseInt(req.query.limit) || 10,
+   },
+   (err, assignments) => {
+     if (err) {
+       res.send(err);
+     }
+     res.send(assignments);
+   }
+ );
 }
 
 // Récupérer un assignment par son id (GET)
